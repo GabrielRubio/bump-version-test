c7292ac968fd54118214a4979cdd9f8ae9c270cf
GabrielRubio <gabriel.rubio.usp@gmail.com>
2019-08-22 15:28:37 -0300

    update bump_version

--- bump-version.sh
+++ bump-version.sh
@@ -1,17 +1,47 @@
 # mudar version de um projeto
 #!/bin/bash
+echo "|"
+echo "Change to develop Branch!"
+git checkout develop -q
 
+echo "|"
+echo "Update develop Branch!"
+git pull origin develop
+
+echo "|"
+echo "App Current Version: "
+cat package.json | grep \"version\"
+
+echo "|"
 read -p "Enter New Version: " version
-echo "Welcome $version!"
+echo "Version $version"
 
-# git checkout develop
-# git pull origin develop
-# git checkout -b release-$version
-# yarn version --new-version $version
-# git checkout master
-# git merge --no-ff release-$version
-# git checkout develop
-# git merge --no-ff release-$version
+echo "|"
+echo "Create new release branch!"
+git checkout -b release-$version
+
+
+echo "|"
+echo "Bump Version with yarn!"
+yarn version --new-version $version
+
+
+echo "|"
+echo "Mergin with master!"
+git checkout master
+git merge --no-ff release-$version
+
+echo "|"
+echo "Mergin with develop!"
+git checkout develop
+git merge --no-ff release-$version
+
+echo "|"
+echo "Removign release branch!"
+git branch -d release-$version
+
+echo "|"
+echo "Pushing to origin!"
 # git push origin develop
 # git push origin master
 # git push --tags
