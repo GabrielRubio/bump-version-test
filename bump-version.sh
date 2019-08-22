27e5df0b783bb6d8c6bea5f7ae7d21dcda7e0969
GabrielRubio <gabriel.rubio.usp@gmail.com>
2019-08-22 13:51:08 -0300

    add picGo bump-version

--- bump-version.sh
+++ bump-version.sh
@@ -1,13 +1,17 @@
 # mudar version de um projeto
-​
-git checkout develop
-git pull origin develop
-git checkout -b release-$1
-yarn version --new-version $1
-git checkout master
-git merge --no-ff release-$1
-git checkout develop
-git merge --no-ff release-$1
-git push origin develop
-git push origin master
-git push --tags
+#!/bin/bash
+
+read -p "Enter New Version: " version
+echo "Welcome $version!"
+
+# git checkout develop
+# git pull origin develop
+# git checkout -b release-$version
+# yarn version --new-version $version
+# git checkout master
+# git merge --no-ff release-$version
+# git checkout develop
+# git merge --no-ff release-$version
+# git push origin develop
+# git push origin master
+# git push --tags
